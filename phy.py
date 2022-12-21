ad87820abe121fb98da17709553f232511cb7d47
haoching <haoching929@gmail.com>
2022-12-21 06:22:48 +0000

    教學

--- phy.py
+++ phy.py
@@ -2,20 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+x_name = input("輸入 x 軸名稱")
+y_name = input("輸入 y 軸名稱")
 
 
-x, y = np.loadtxt(f'test/graph/interference_data.csv', delimiter = ',', usecols = (0, 1), unpack = True)
+
+x, y = np.loadtxt(f'interference_data.csv', delimiter = ',', usecols = (0, 1), unpack = True)
 
 A = np.vstack([x, np.ones(len(x))]).T
-m, c = np.linalg.lstsq(A, y, rcond = -1)[0]
+m, c = np.linalg.lstsq(A, y, rcond = -1)[0] # 計算回歸直線
 print(m, c)
 
 fig, (graph) = plt.subplots(1, 1, sharex = False, sharey = True, figsize = (4.5, 4.5))
 
-(graph).set_xlabel("incidence ", fontsize = 14)
-(graph).set_ylabel("refraction", fontsize = 14)
+(graph).set_xlabel(x_name, fontsize = 14)#字體大小
+(graph).set_ylabel(y_name, fontsize = 14)
 (graph).grid(color = 'red', linestyle = '--', linewidth = 1)
-(graph).set_xlim(0, np.max(x) * 1.1)
+(graph).set_xlim(0, np.max(x) * 1.1) #xy軸極值大小
 (graph).set_ylim(0, np.max(y) * 1.1)
 (graph).plot(x, y, color = 'blue', marker = 'o', markersize = 4, linestyle = '', label = "Original Data")
 (graph).plot(x, m * x + c, color = 'orange', linewidth = 2, label = "Fitted Line")
@@ -23,4 +26,3 @@ fig, (graph) = plt.subplots(1, 1, sharex = False, sharey = True, figsize = (4.5,
 
 fig.savefig('graph.svg')
 fig.savefig('graph.png')
-fig.show()
--- readme.md
+++ readme.md
@@ -1,6 +1,25 @@
 # 物理探究
 
+## 使用教學
+
+溫馨提醒要先下載 python
+
+### 方法一 git
+- 下載 git
+- 到 git bash 輸入指令
+```
+git clone https://github.com/haoching/physics-graph.git
+```
+### 方法二
+
+- 直接在右上下載 .zip 檔
+- 解壓縮
+- 快樂作圖
+
 ## 套件
+
+使用之前要先載套件
+
 ### matplotlib
 ```
 pip3 install matplotlib 
@@ -9,3 +28,6 @@ pip3 install matplotlib
 ```
 pip3 install numpy
 ```
+
+###　開始執行
+接下來直接執行
